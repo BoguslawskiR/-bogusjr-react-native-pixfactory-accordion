5643f4e70aa5dd6748b310cd6985a1b3462d374a
radek <bogusjr26@gmail.com>
2018-02-03 11:32:48 +0100

    init

---
+++ PixAccordion.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { View, TouchableHighlight } from 'react-native';
+import Collapsible from 'react-native-collapsible';
+
+/**
+ * PixAccordion Component
+ */
+class PixAccordion extends Component {
+  static propTypes = {
+    collapsed: PropTypes.bool,
+    underlayColor: PropTypes.string,
+    renderHeader: PropTypes.func.isRequired,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    underlayColor: 'rgba(0, 0, 0, 0)',
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: props.collapsed !== undefined ? props.collapsed : true,
+    };
+  }
+
+  _toggleCollapsed() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.collapsed !== this.state.collapsed) {
+      this.setState({
+        collapsed: nextProps.collapsed
+      });
+    }
+  }
+
+  render() {
+    const { children, renderHeader, underlayColor, ...collapsibleProps } = this.props;
+    return (
+      <View>
+        <TouchableHighlight
+          onPress={() => this._toggleCollapsed()}
+          underlayColor={underlayColor}
+        >
+          <View>
+            {renderHeader({ isOpen: this.state.collapsed })}
+          </View>
+        </TouchableHighlight>
+        <Collapsible
+          {...collapsibleProps }
+          collapsed={this.state.collapsed}
+        >
+          {children}
+        </Collapsible>
+      </View>
+    );
+  }
+}
+
+export default PixAccordion;
